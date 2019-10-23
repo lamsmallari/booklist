028e0d8138b9dd10ed7115ca9369b06a2afac80c
Lam Mallari <lamsmallarijr@gmail.com>
2019-10-23 18:14:32 +0800

    Updates

---
+++ precache-manifest.0cbd98e20c239eee37ae2c42588a2cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13212c66e1a7dfbcf868dc0b205cffc3",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "efe640699c554433096b",
+    "url": "/booklist/static/css/main.d9741567.chunk.css"
+  },
+  {
+    "revision": "58738085ede3a80b2e9e",
+    "url": "/booklist/static/js/2.2496c127.chunk.js"
+  },
+  {
+    "revision": "efe640699c554433096b",
+    "url": "/booklist/static/js/main.b9f27866.chunk.js"
+  },
+  {
+    "revision": "3f93df9ef5007bda9f57",
+    "url": "/booklist/static/js/runtime-main.a433f354.js"
+  }
+]);
